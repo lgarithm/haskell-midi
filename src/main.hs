ac1f9dfd7f4fe0d4e197cc880a53089eca0b44e5
lgarithm <lgarithm@gmail.com>
2014-12-09 20:19:26 +0000

    use Word8

--- src/main.hs
+++ src/main.hs
@@ -2,6 +2,11 @@ module Main where
 import System.Environment
 import Midi
 
+show_chunk (MidiHeadChunk x y z) = unlines $ map show $ zip ["format", "ntrks", "division"] [x, y, z]
+show_chunk (MidiTrackChunk len events) = unlines $ (show len) : (map show events)
+
+instance Show MidiFile where show (MidiFile chunks)= unlines $ map show_chunk chunks
+
 main = do { file:_ <- getArgs
           ; midi <- pMidiFromFile file
-          ; print midi }
+          ; print . sure $ midi }
